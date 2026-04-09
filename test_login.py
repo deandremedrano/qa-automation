4e279769a6c4bd961d05544b78c86f77d3111962
Deandre Medrano <medrano84deandre@gmail.com>
2026-04-09 10:08:01 -0700

    Add GitHub Actions CI/CD pipeline

--- test_login.py
+++ test_login.py
@@ -13,6 +13,9 @@ VALID_PASSWORD = "SuperSecretPassword!"
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
